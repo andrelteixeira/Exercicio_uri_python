0c1a936fa881ea99d15dc7d8e898cd43f2201f94
André Teixeira <andrelteixeira@icloud.com>
2022-04-08 20:36:41 -0300

    exercicios 1000 a 1009

---
+++ 1008.py
@@ -0,0 +1,6 @@
+numFunc = int(input())
+numHoras = int(input())
+valor = float(input())
+salario = numHoras*valor
+print("NUMBER =", numFunc)
+print("SALARY = U$ %.2f" % salario)
---
+++ 1009.py
@@ -0,0 +1,5 @@
+nome = input()
+salarioFixo = float(input())
+vendas = float(input())
+salario = salarioFixo+vendas*0.15
+print("TOTAL = R$ %.2f" % salario)
